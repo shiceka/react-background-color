434c7bd4ca5a44932e9a53ebbfcd3ab179d25245
shiceka <shicekaakhona@gmail.com>
2022-06-02 13:26:58 +0200

    added increment,decremrnt and counter buttons

--- react-background-color-change/src/App.js
+++ react-background-color-change/src/App.js
@@ -10,37 +10,65 @@
 // }
 
 // export default App;
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    // this.state = {backgroundColor: "green"}
-    this.state = {backgroundColor: "green"}
+    this.state = { backgroundColor: "green", count: 0 };
     this.handleClick = this.handleClick.bind(this);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
   }
-  handleClick(){
-    // this.setState({backgroundColor: "blue"})
-    this.setState(state => {
-      if(state.backgroundColor === "blue"){
-        return {backgroundColor: "green"}
-      }
-      else if(state.backgroundColor === "green") {
-        return {backgroundColor: "blue"}
-      }
-    })
+
+  increment() {
+    this.setState((state) => ({
+      count: state.count + 1
+    }));
   }
+
+  decrement() {
+    this.setState((state) => { 
+      if(state.count <= 0){
+        return 0;
+      }
+      else{
+      return {count: state.count - 1} 
+    }});
+  }
+
+  handleClick() {
+    this.setState((state) => {
+      if (state.backgroundColor === "blue") {
+        return { backgroundColor: "green" };
+      } else if (state.backgroundColor === "green") {
+        return { backgroundColor: "blue" };
+      }
+    });
+  }
+
   render() {
-    if(this.state.backgroundColor){
-      return(<div style={{backgroundColor: this.state.backgroundColor , height:"100vh", paddingTop: '20rem'}}><button onClick={this.handleClick} className="btn">Click Me</button></div>)
+    if (this.state.backgroundColor) {
+      return (
+        <div
+          style={{
+            backgroundColor: this.state.backgroundColor,
+            height: "100vh",
+            paddingTop: "20rem",
+          }}
+        >
+          <h1>{this.state.count}</h1>
+          <button onClick={this.increment} className="btn" >
+            Counter-Plus
+          </button><br />
+          <button onClick={this.handleClick} className="btn">
+            Background
+          </button><br />
+          <button onClick={this.decrement} className="btn">
+            Counter-Minus
+          </button>
+        </div>
+      );
     }
-    // else{
-    //   return(<div style={{backgroundColor: this.state.backgroundColor , height:"100vh"}}><button onClick={this.handleClick} className="btn">Click Me1</button></div>)
-    // }
-    
-    // return (
-    //   <div style={{backgroundColor: this.state.backgroundColor , height:"100vh"}}><button onClick={this.handleClick} >Click Me</button></div>
-    // )
   }
 }
-
